98c31048f315ef671b46d7a9f2a7ccd9afd4dec0
monkeycs60 <clement.serizay@gmail.com>
2023-04-06 22:41:30 +0200

    🔧 chore(front-end): add eslint configuration and dependencies This commit adds a new file `.vscode/settings.json` which configures the editor to run eslint on save and a new file `front-end/.eslintrc.json` which contains the eslint configuration for the front-end project. The `front-end/package.json` file was also updated to include the necessary eslint dependencies.
    🐛 fix(App.tsx): remove unused variable and fix indentation
    The variable `userOne` is not used and can be safely removed. The indentation of the code has also been fixed to improve readability.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,28 +1,31 @@
-import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { userSliceProps } from './redux/types';
-import { setUsername, setUserDatas } from './redux/userSlice';
-import { useState } from 'react';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { userSliceProps } from "./redux/types";
+import { setUsername, setUserDatas } from "./redux/userSlice";
+import { useState } from "react";
 
 function App() {
   const dispatch = useDispatch();
-  const user  = useSelector((state: userSliceProps) => state);
+  const userOne = "Pierr";
+
+  const user = useSelector((state: userSliceProps) => state);
   console.log(user);
 
   const [userDatas, setUserDatasState] = useState<string>("");
-  
+
   return (
     <div className="App">
-   <h1>salut les gens</h1>
-  <input type="text" 
-  value={user.username}
-  onChange={(e) => setUserDatasState(e.target.value)}
-  />
-    <button onClick={
-      () => dispatch(setUsername(userDatas))
-    }>update user</button>
+      <h1>salut les gens</h1>
+      <input
+        type="text"
+        value={user.username}
+        onChange={(e) => setUserDatasState(e.target.value)}
+      />
+      <button onClick={() => dispatch(setUsername(userDatas))}>
+        update user
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
